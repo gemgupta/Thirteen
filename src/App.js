55c0b82ea15cd952c94366691c4c4ed4b3c49369
gemgupta <gemgupta007@gmail.com>
2023-12-25 17:22:21 +0530

    with Action Thunk

--- src/App.js
+++ src/App.js
@@ -4,59 +4,18 @@ import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
-import { CartActions } from "./Store/CartSlice";
+import { sendCartData } from "./Store/CartItemSlice";
 let isInitial = true;
 function App() {
   const cart = useSelector((state) => state.CartItems);
   const responseNotification = useSelector((state) => state.Cart.responses);
   const dispatch = useDispatch();
   useEffect(() => {
-    const sendingReq = async () => {
-      dispatch(
-        CartActions.responseNotification({
-          status: "Pending",
-          message: "Sending Cart Data",
-          title: "sending",
-        })
-      );
-      const response = await fetch(
-        "https://expensetracker-69a6d-default-rtdb.firebaseio.com/cart.json",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      if (!response.ok) {
-        dispatch(
-          CartActions.responseNotification({
-            status: "error",
-            message: "Sending Cart Data Failed",
-            title: "Sending Failed",
-          })
-        );
-      } else {
-        dispatch(
-          CartActions.responseNotification({
-            status: "success",
-            message: "Items Added to Cart",
-            title: "Sent",
-          })
-        );
-      }
-    };
     if (isInitial) {
       isInitial = false;
       return;
     }
-    sendingReq().catch((error) => {
-      dispatch(
-        CartActions.responseNotification({
-          status: "error",
-          message: "Sending Cart Data Failed",
-          title: "Sending Failed",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
   return (
     <>
